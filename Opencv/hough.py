ec91584b43ab9159b7201f17bc98def7e4e0de47
이동규 <wind.dong.dream@gmail.com>
2020-08-15 16:16:28 +0900

    HoughP 구현

--- Opencv/hough.py
+++ Opencv/hough.py
@@ -13,22 +13,25 @@ canny = cv2.Canny(gray, 2500, 1500, apertureSize=5, L2gradient=True)
 plt.imshow(canny)
 plt.show()
 
-lines = cv2.HoughLines(canny, 0.8, (np.pi) / 90, 100, srn=100, stn=200, min_theta=0, max_theta=np.pi)
+# lines = cv2.HoughLines(canny, 1, (np.pi) / 180, 100)
+lines = cv2.HoughLinesP(canny, 1, np.pi / 360, 120, minLineLength=200, maxLineGap=200)
 
 for i in lines:
-    rho, theta = i[0]
-    a, b = np.cos(theta), np.sin(theta)
-    x0, y0 = a * rho, b * rho
-
-    scale = src.shape[0] + src.shape[1]
-
-    x1 = int(x0 + scale * -b)
-    y1 = int(y0 + scale * a)
-    x2 = int(x0 - scale * -b)
-    y2 = int(y0 - scale * a)
-
-    cv2.line(dst, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    cv2.circle(dst, (x0, y0), 3, (255, 0, 0), 5, cv2.FILLED)
+    x1, y1, x2, y2 = i[0]
+    cv2.line(dst, (x1, y1), (x2, y2), (0, 0, 255), 3, cv2.LINE_AA)
+    # rho, theta = i[0]
+    # a, b = np.cos(theta), np.sin(theta)
+    # x0, y0 = a * rho, b * rho
+    #
+    # scale = src.shape[0] + src.shape[1]
+    #
+    # x1 = int(x0 + scale * -b)
+    # y1 = int(y0 + scale * a)
+    # x2 = int(x0 - scale * -b)
+    # y2 = int(y0 - scale * a)
+    #
+    # cv2.line(dst, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    # cv2.circle(dst, (x0, y0), 3, (255, 0, 0), 5, cv2.FILLED)
 
 plt.imshow(dst)
 plt.show()
