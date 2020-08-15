ff42ad3d60ba353d7b7ca2db3ea66f969d513a5c
이동규 <wind.dong.dream@gmail.com>
2020-08-15 19:54:03 +0900

    OpenPose 추가
    Former-commit-id: 3db7478617a84db0f2bc14edcff52aedbe1733d4

--- aktwelve_mask_rcnn/process/image_api.py
+++ aktwelve_mask_rcnn/process/image_api.py
@@ -1,5 +1,9 @@
 import numpy as np
 import cv2
+import sys
+import os
+from sys import platform
+import argparse
 
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
@@ -90,4 +94,78 @@ def recommend_object_position(center_point, image, is_person=False):
     print('center_point', center_point)
     print('image', image.shape)
     print('is_person', is_person)
+
+    try:
+        # Import Openpose (Windows/Ubuntu/OSX)
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        try:
+            # Windows Import
+            if platform == "win32":
+                # Change these variables to point to the correct folder (Release/x64 etc.)
+                # 윈도우 코드는 테스트 안해봐서 안될 수도 있음 경로를 적절히 수정해줘야함...
+                sys.path.append(dir_path + '../openpose/python/openpose/Release')
+                os.environ['PATH'] = os.environ['PATH'] + ';' + dir_path + '/../../x64/Release;' + dir_path + '/../../bin;'
+                import pyopenpose as op
+            else:
+                # Change these variables to point to the correct folder (Release/x64 etc.)
+                sys.path.append(dir_path + '/../openpose/build/python')
+
+                print(os.getcwd())
+                for path in sys.path:
+                    print(path)
+                # If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the OpenPose/python module from there. This will install OpenPose and the python library at your desired installation path. Ensure that this is in your python path in order to use it.
+                # sys.path.append('/usr/local/python')
+                from openpose import pyopenpose as op
+        except ImportError as e:
+            print(
+                'Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
+            raise e
+
+        # Flags
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--image_path", default="../../../examples/media/COCO_val2014_000000000192.jpg",
+                            help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
+        args = parser.parse_known_args()
+
+        # Custom Params (refer to include/openpose/flags.hpp for more parameters)
+        params = dict()
+        params["model_folder"] = "../openpose/models/"
+
+        # Add others in path?
+        for i in range(0, len(args[1])):
+            curr_item = args[1][i]
+            if i != len(args[1]) - 1:
+                next_item = args[1][i + 1]
+            else:
+                next_item = "1"
+            if "--" in curr_item and "--" in next_item:
+                key = curr_item.replace('-', '')
+                if key not in params:  params[key] = "1"
+            elif "--" in curr_item and "--" not in next_item:
+                key = curr_item.replace('-', '')
+                if key not in params: params[key] = next_item
+
+        # Construct it from system arguments
+        # op.init_argv(args[1])
+        # oppython = op.OpenposePython()
+
+        # Starting OpenPose
+        opWrapper = op.WrapperPython()
+        opWrapper.configure(params)
+        opWrapper.start()
+
+        # Process Image
+        datum = op.Datum()
+        # imageToProcess = cv2.imread(image)
+        imageToProcess = image
+        datum.cvInputData = imageToProcess
+        opWrapper.emplaceAndPop([datum])
+
+        # Display Image
+        print("Body keypoints: \n" + str(datum.poseKeypoints))
+        cv2.imshow("OpenPose 1.6.0 - Tutorial Python API", datum.cvOutputData)
+        cv2.waitKey(0)
+    except Exception as e:
+        print(e)
+        sys.exit(-1)
     return ""
