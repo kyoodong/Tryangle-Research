ecd6867be6dc25832a37b52ef96d48de213d5c07
이동규 <wind.dong.dream@gmail.com>
2020-08-15 19:22:06 +0900

    객체 질량중심 구하기 구현

--- aktwelve_mask_rcnn/lines/layers.py
+++ aktwelve_mask_rcnn/lines/layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
@@ -10,50 +11,79 @@ dy = [-1, 0, 1, 0]
 # y : 탐색을 시작할 y좌표
 # visits : 해당 픽셀을 방문했는지 여부를 저장하는 2차원 boolean 배열
 # threshold : threshold(0 ~ 1 사이의 값) 크기 이상의 객체만을 처리하며, 그 이하인 경우 없는 취급함
-def bfs(image, x, y, visits, threshold):
+def __get_contour_center_point(image, x, y, visits, threshold):
     layered_image = np.zeros_like(image)
     queue = []
-    queue.append((x, y))
-    visits[x][y] = True
+    queue.append((y, x))
+    visits[y][x] = True
 
     count = 1
+
+    # 무게 중심 변수
+    contour = list()
+
+    # 질량 중심 변수
+    x_sum = x
+    y_sum = y
+    x_count = 1
+
     while len(queue) > 0:
         position = queue.pop()
 
         for i in range(4):
-            X = position[0] + dx[i]
-            Y = position[1] + dy[i]
+            X = position[1] + dx[i]
+            Y = position[0] + dy[i]
 
             if X < 0 or X >= image.shape[0] or Y < 0 or Y >= image.shape[1]:
                 continue
 
-            if image[X][Y]:
-                if not visits[X][Y]:
-                    visits[X][Y] = True
+            if image[Y][X]:
+                if not visits[Y][X]:
+                    visits[Y][X] = True
                     count += 1
-                    queue.append((X, Y))
+                    queue.append((Y, X))
 
             else:
-                layered_image[X][Y] = 1
+                # 무게 중심을 구하기 위한 contour 수집
+                contour.append((X, Y))
+
+                # 질량 중심을 구하기 위한 각 좌표 합
+                x_sum += X
+                y_sum += Y
+                x_count += 1
+
+                layered_image[Y][X] = 1
     total = image.shape[0] * image.shape[1]
     if count > total * threshold:
-        return layered_image
-    return np.zeros_like(layered_image)
+        ### 무게 중심
+        # M = cv2.moments(np.array(contour))
+        # cx = int(M['m10'] / M['m00'])
+        # cy = int(M['m01'] / M['m00'])
+
+        ### 질량 중심
+        cx = int(x_sum / x_count)
+        cy = int(y_sum / x_count)
+        return layered_image, (cx, cy)
+    return np.zeros_like(layered_image), None
 
 
-# 외곽선만 따는 함수
-def to_lines(image, threshold):
+# 외곽선 + 오브젝트의 무게중심을 구해주는 함수
+def get_contour_center_point(image, threshold):
     channels = image.shape[-1]
     width = image.shape[0]
     height = image.shape[1]
     layered_image = np.zeros_like(image)
+    cogs = list()
     for i in range(channels):
         visits = np.zeros_like(image[:, :, i])
         for w in range(width):
             for h in range(height):
-                if image[w][h][i] and not visits[w][h]:
+                if image[h][w][i] and not visits[h][w]:
                     # bfs 를 돌려서 외곽선 탐색
-                    layered_image[:, :, i] += bfs(image[:, :, i], w, h, visits, threshold)
-    return layered_image
+                    l, cog = __get_contour_center_point(image[:, :, i], w, h, visits, threshold)
+                    layered_image[:, :, i] += l
+                    if cog:
+                        cogs.append(cog)
+    return layered_image, cogs
 
 
