93087a35294d9152a9202775c086f1680c736e7d
이동규 <wind.dong.dream@gmail.com>
2020-08-16 21:32:27 +0900

    수평, 수직선만 검출하도록 수정
    Former-commit-id: 66274f66ae1f109777f99baf1e8d4b2bb277c4ee [formerly a3b4ed353ea6af418dddf76e6a8f5fa5d8e6c91b] [formerly faf26606d0c7331d69112e0a59c6a2c2f298696a [formerly 6a4e1102561d75d1ecd1816b6564ff0c5c9c4103]]
    Former-commit-id: 979f5f566ff038a1bc23a3cd831f1f0c82c98c95 [formerly 42c018ed91df4e3a0cb27b9f4e21ce47b94849a4]
    Former-commit-id: 0a48a843dd797cc016133d1e6986d57a547e2184

--- aktwelve_mask_rcnn/process/hough.py
+++ aktwelve_mask_rcnn/process/hough.py
@@ -17,11 +17,21 @@ def find_hough_line(image):
     # lines = cv2.HoughLines(canny, 1, (np.pi) / 180, 100)
     lines = cv2.HoughLinesP(canny, 1, np.pi / 360, 120, minLineLength=200, maxLineGap=200)
 
+    # 수평선에서는 좌우, 수직선에서는 상하의 점의 수평 차이가 gamma 미만이면 수평, 수직선이라 판단하고 가이드를 제공
+    gamma = 25
     result = list()
     for line in lines:
+        # HoughP
         x1, y1, x2, y2 = line[0]
-        result.append(line[0])
-        cv2.line(dst, (x1, y1), (x2, y2), (0, 0, 255), 3, cv2.LINE_AA)
+
+        # 수평, 수직선만 검출
+        if abs(y1 - y2) < gamma or abs(x1 - x2) < gamma:
+            result.append(line[0])
+
+        # cv2.line(dst, (x1, y1), (x2, y2), (0, 0, 255), 3, cv2.LINE_AA)
+
+
+        # Hough
         # rho, theta = i[0]
         # a, b = np.cos(theta), np.sin(theta)
         # x0, y0 = a * rho, b * rho
