57075b658f08f5c485798a74344ebcc6f1e14f02
이동규 <wind.dong.dream@gmail.com>
2020-08-25 15:19:42 +0900

    coco Dataset image id 중복될 수 있던 버그 수정
    Former-commit-id: 3ddd553138a2c07f082852d62999733a14963332 [formerly b87c4aca848d3369ecc4cd4f0312379d3bb60de7] [formerly 2d0be70f597c4ac508446c9a1f8a778529beb143 [formerly 62032453578c139b195244055e4f758bc5d65316]]
    Former-commit-id: 009b0acbee1abb524d28b74845fde0223297430c [formerly aa1f05956f559b87536229a73fde8fdd348616e4]
    Former-commit-id: 456f233125528af9ac0f4d833f5fc7daf06d3ebd

--- DataFormatConverter/sky_finder_to_coco.py
+++ DataFormatConverter/sky_finder_to_coco.py
@@ -14,54 +14,54 @@ with open("datasets/coco_annotations/instances_val2017.json") as coco_json_file:
 
     category_id = coco.add_category('background', 'sky')
 
-    # max_num = 10000
-    # for mask_image_file in mask_image_files:
-    #     print('process', mask_image_file)
-    #     number = mask_image_file.split('/')[-1]
-    #     number = number.split('.png')[0]
-    #     sky_image_files = glob('datasets/skyfinder/images/{}000*.jpg'.format(number))
-    #
-    #     added_image_ids = list()
-    #     count = 0
-    #     for sky_image_file in sky_image_files:
-    #         filename = sky_image_file.split('/')[-1]
-    #         im = cv2.imread(sky_image_file)
-    #         if im is None:
-    #             print('im is None', sky_image_file)
-    #             continue
-    #
-    #         id = int(filename.split('.')[0])
-    #         if coco.is_exist_image_id(id):
-    #             print('{} is already used'.format(id))
-    #             continue
-    #
-    #         coco.add_image(1, filename, None, im.shape[0], im.shape[1], None, None, id)
-    #         added_image_ids.append(id)
-    #
-    #         count += 1
-    #         if count == max_num:
-    #             break
-    #
-    #     mask_image = cv2.imread(mask_image_file)
-    #     mask_image = cv2.cvtColor(mask_image, cv2.COLOR_BGR2GRAY)
-    #     _, mask_image = cv2.threshold(mask_image, 127, 255, 0)
-    #
-    #     contours, hierarchy = cv2.findContours(mask_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #     segmentation = []
-    #     for contour in contours:
-    #         contour = contour.flatten().tolist()
-    #         # segmentation.append(contour)
-    #         if len(contour) > 4:
-    #             segmentation.append(contour)
-    #     if len(segmentation) == 0:
-    #         continue
-    #
-    #     rle = mask.encode(np.asfortranarray(mask_image))
-    #     area = mask.area(rle)
-    #     bbox = mask.toBbox(rle)
-    #
-    #     for added_image_id in added_image_ids:
-    #         coco.add_annotation(segmentation, area, 0, added_image_id, [int(v) for v in bbox], category_id)
+    max_num = 10000
+    for mask_image_file in mask_image_files:
+        print('process', mask_image_file)
+        number = mask_image_file.split('/')[-1]
+        number = number.split('.png')[0]
+        sky_image_files = glob('datasets/skyfinder/images/{}000*.jpg'.format(number))
+
+        added_image_ids = list()
+        count = 0
+        for sky_image_file in sky_image_files:
+            filename = sky_image_file.split('/')[-1]
+            im = cv2.imread(sky_image_file)
+            if im is None:
+                print('im is None', sky_image_file)
+                continue
+
+            id = int(filename.split('.')[0])
+            if coco.is_exist_image_id(id):
+                print('{} is already used'.format(id))
+                continue
+
+            coco.add_image(1, filename, None, im.shape[0], im.shape[1], None, None, id)
+            added_image_ids.append(id)
+
+            count += 1
+            if count == max_num:
+                break
+
+        mask_image = cv2.imread(mask_image_file)
+        mask_image = cv2.cvtColor(mask_image, cv2.COLOR_BGR2GRAY)
+        _, mask_image = cv2.threshold(mask_image, 127, 255, 0)
+
+        contours, hierarchy = cv2.findContours(mask_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        segmentation = []
+        for contour in contours:
+            contour = contour.flatten().tolist()
+            # segmentation.append(contour)
+            if len(contour) > 4:
+                segmentation.append(contour)
+        if len(segmentation) == 0:
+            continue
+
+        rle = mask.encode(np.asfortranarray(mask_image))
+        area = mask.area(rle)
+        bbox = mask.toBbox(rle)
+
+        for added_image_id in added_image_ids:
+            coco.add_annotation(segmentation, area, 0, added_image_id, [int(v) for v in bbox], category_id)
 
     print('작성 중...')
     f = open("val_annotation.json", "w")
