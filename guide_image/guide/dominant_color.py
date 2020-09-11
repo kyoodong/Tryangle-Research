26fdc5ca935020ce47ab87fa157f1a8db2585950
이강준 <ladun9128@gmail.com>
2020-09-11 15:52:36 +0900

    guide_image.py에 있던 기능들을 분할 guide_image.py의 있던 기능들을 굳이 클래스로 안 만들어도 될거 같아서 분할함 dominant_color.py -> knn으로 주요색을 찾아내고, 비교하는 함수가 있음 vp_detection.py -> 소실점을 찾는 코드
    demo.py의 코드도 위 두개의 예시를 볼 수 있게 수정
    
    
    Former-commit-id: 5aeaf54012968ee48a678ec4ae6060db4a389165 [formerly 04f3eae4b95ef03fc5e99c1a4ff15ebb90de0d06] [formerly 871f203a71518ad46625bea2bbd955d2c9dd23c3 [formerly c813dc5e5868586311201cd8bf5ebb17c693e47d]]
    Former-commit-id: 96c8182a6814f103d94876c5fdd8abed5579dd01 [formerly f7a7394e5a3b22064711c35c1456a49f38e1f6dc]
    Former-commit-id: ef125c2845a3b3d96085833d83c6415947c3a631

---
+++ guide_image/guide/dominant_color.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+from guide.utils import ciede2000 as color_diff
+
+def find_similar_by_color(query_color, other_colors, count=3):
+    if count > len(other_colors):
+        count = len(other_colors)
+
+    diff = diff_color(query_color, other_colors)
+    diff_arg = np.argsort(diff)
+
+    return diff_arg[:count]
+
+def diff_color(query_color, other_colors):
+    diff = []
+    ciede2000 = color_diff.ciede2000
+    rgb2lab = color_diff.rgb2lab
+
+    print("Query", query_color)
+    print("Other", other_colors)
+
+    for pallet in other_colors:
+        color_diff_sum = 0
+        for o_color in pallet:
+            color_diff_sum += np.min([ciede2000(rgb2lab(q_color), rgb2lab(o_color)) for q_color in query_color])
+        diff.append(color_diff_sum)
+    return np.array(diff)
+
+#########################################
+# 주요 색 찾기
+#########################################
+def get_dominant_color(img, n_color=4, image_processing_size=(32,32)):
+    image = cv2.resize(img, image_processing_size)
+    pixels = np.float32(image.reshape(-1, 3))
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
+    flags = cv2.KMEANS_RANDOM_CENTERS
+    a, labels, pallets = cv2.kmeans(pixels, n_color, None, criteria, 10, flags)
+    b, counts = np.unique(labels, return_counts=True)
+    counts_argsort = np.argsort(counts)[::-1]
+
+    return pallets[counts_argsort]
