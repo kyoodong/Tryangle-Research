e59a424d6ac751676268a7703e253ea79cb71c1c
이강준 <ladun9128@gmail.com>
2020-09-22 20:26:54 +0900

    주요색 찾는 코드를 삭제해서 demo.py 수정
    Former-commit-id: 1ebd5330455f2c3b42b52406faa5cd431a2e7f5c [formerly 3c0d823b012b8e68c3bcc1235ecb72274b35cbfe]
    Former-commit-id: 246564318c8f351aef21f46933ccd4ecf7ea6c17

--- guide_image/demo.py
+++ guide_image/demo.py
@@ -2,39 +2,20 @@ import numpy as np
 import cv2
 import glob
 
-from guide.utils import drawer
-from guide import dominant_color
-from guide.vp_detection import find_vps
+from retrieval.utils import drawer
+from retrieval.vp_detection import find_vps
 
 
 test_image = [4]
-dominant_colors = []
+imgs = []
 for i in test_image:
     path = f"image/test/image{i}.jpg"
     img = cv2.imread(path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    color = dominant_color.get_dominant_color(img)
-    dominant_colors.append([img, color])
+    imgs.append(img)
 
-vp = find_vps(dominant_colors[0][0])
+vp = find_vps(imgs[0])
 
 drawer.display("Query",
-               dominant_colors[0][0],
+               imgs[0],
                vanishing_point=vp)
-drawer.draw_color(dominant_colors[0][1])
-
-# 색감이 비슷한 사진 찾기
-list_img = []
-for file in glob.glob("image/similar_test/*.jpg"):
-    img = cv2.imread(file)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    color = dominant_color.get_dominant_color(img)
-    list_img.append([img, color])
-
-
-similar_img_arg = dominant_color.find_similar_by_color(dominant_colors[0][1], np.array(list_img)[:, 1], count=5)
-
-for arg in similar_img_arg:
-    drawer.display(f"Result{arg}", list_img[arg][0])
-    drawer.draw_color(list_img[arg][1])
-
