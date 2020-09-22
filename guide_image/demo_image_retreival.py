5e6d4c7397f1c3ca59b6ef657652205b8a9fa123
이강준 <ladun9128@gmail.com>
2020-09-22 20:09:39 +0900

    주요색 찾는 코드 삭제, 이미지 검색 모델 MobileNetV2로 변경
    Former-commit-id: a0e66d968a33fff895e12862f5acb37c8f295e9b [formerly 997deb957399792535e1f640e8ec299ec8eed894] [formerly 71419271f40372b2edd22082ab46507eaadc93df [formerly 712feb3a24f7d134db676a54687a66d2c7bf743c]]
    Former-commit-id: f6ad31bb603cd8c82f87a28aa84e59d678597e58 [formerly e9688c4e838b18149f9a1e60f1a8da6884b8eed9]
    Former-commit-id: 04905d583115100b91043f1eaf6941dc8e505189

--- guide_image/demo_image_retreival.py
+++ guide_image/demo_image_retreival.py
@@ -1,6 +1,6 @@
 import numpy as np
+import time
 
-import tensorflow_hub as hub
 import tensorflow as tf
 import tensorflow.keras.layers as layers
 from tensorflow.keras.models import Model
@@ -15,7 +15,7 @@ def retrieval_res(image_path):
     # 현재는 간단하게 MobileNetV2로 이미지의 feature를 뽑아내서 비교
     dim = 2048
     input_shape = (224, 224, 3)
-    base = tf.keras.applications.ResNet152(input_shape=input_shape,
+    base = tf.keras.applications.MobileNetV2(input_shape=input_shape,
                                              include_top=False,
                                              weights='imagenet')
     base.trainable = False
@@ -24,9 +24,12 @@ def retrieval_res(image_path):
     # 이미지 로드
     img = preprocess(image_path, input_shape)
 
+    st = time.time()
     # 이미지에서 feature 뽑아내기
     fvec = model.predict(np.array([img]))
+    print(f"[INFO] feature extract time {time.time() - st}")
 
+    st = time.time()
     # 이미지 검색 클래스 생성
     # fvecs.bin랑 fnames.txt는 feature_extractor.py에서 만들 수 있는 파일
     imageRetrieval = ImageRetrieval(fvec_file="guide/fvecs.bin",
@@ -34,40 +37,8 @@ def retrieval_res(image_path):
                                     fvec_dim=dim)
 
     results = imageRetrieval.search(fvec)
+    print(f"[INFO] image retrieval time {time.time() - st}")
 
-    for path, dst in results:
-        print(f"Image path: {path}, dst: {dst}")
-
-
-def retrieval_delf(image_path):
-    def get_image(img_path):
-        img = tf.io.read_file(img_path)
-        img = tf.image.decode_jpeg(img, channels=3)
-        img = tf.image.convert_image_dtype(img, tf.float32)
-        return img
-
-    delf = hub.load('https://tfhub.dev/google/delf/1').signatures['default']
-
-    img = get_image(image_path)
-
-    delf_rt = delf(image=img,
-                   score_threshold=tf.constant(100.0),
-                   image_scales=tf.constant([0.25, 0.3536, 0.5, 0.7071, 1.0, 1.4142, 2.0]),
-                   max_feature_num=tf.constant(1000))
-
-    descrip = delf_rt['descriptors']
-    descrip = np.array(descrip)
-    if descrip.shape[0] < 300:
-        print(f"[INFO] {image_path} feature size: {descrip.shape}")
-        return
-    descrip = descrip[:300]
-    descrip = descrip.flatten().reshape(1, -1)
-
-    imageRetrieval = ImageRetrieval(fvec_file="guide/fvecs_delf.bin",
-                                    fvec_img_file_name="guide/fvecs_delf_names.txt",
-                                    fvec_dim=12000)
-
-    results = imageRetrieval.search(descrip)
     for path, dst in results:
         print(f"Image path: {path}, dst: {dst}")
 
--- guide_image/guide/feature_extractor.py
+++ guide_image/guide/feature_extractor.py
@@ -1,7 +1,7 @@
 import struct
 import glob
 import numpy as np
-import tensorflow_hub as hub
+import os
 
 import tensorflow as tf
 import tensorflow.keras.layers as layers
@@ -17,15 +17,19 @@ def preprocess(img_path, input_shape=None):
     img = tf.image.decode_jpeg(img, channels=input_shape[2])
     if input_shape is not None:
         img = tf.image.resize(img, input_shape[:2])
+    img = tf.keras.applications.mobilenet_v2.preprocess_input(img)
     return img
 
-def extract_res(image_dataset, store_file="fvecs"):
-    binary_file = f"{store_file}.bin"
-    name_file = f"{store_file}_names.txt"
+def extract_res(image_dataset, store_folder="features", store_file="fvecs"):
+
+    if not os.path.exists(store_folder):
+        os.mkdir(store_folder)
+    binary_file = f"{store_folder}/{store_file}.bin"
+    name_file = f"{store_folder}/{store_file}_names.txt"
 
     batch_size = 100
     input_shape = (224, 224, 3)
-    base = tf.keras.applications.ResNet152(input_shape=input_shape,
+    base = tf.keras.applications.MobileNetV2(input_shape=input_shape,
                                            include_top=False,
                                            weights='imagenet')
     base.trainable = False
@@ -37,7 +41,7 @@ def extract_res(image_dataset, store_file="fvecs"):
     dataset = ds.batch(batch_size).prefetch(-1)
 
     with open(binary_file, 'wb') as f:
-        for batch in dataset:
+        for i, batch in enumerate(dataset):
             fvecs = model.predict(batch)
 
             # fvecs의 길이 만큼 fmt설정
@@ -47,68 +51,23 @@ def extract_res(image_dataset, store_file="fvecs"):
             # struct.pack을 사용하여 패킹한다.
             f.write(struct.pack(fmt, *(fvecs.flatten())))
 
-    with open(name_file, 'w') as f:
-        f.write('\n'.join(fnames))
-
-def get_image(img_path):
-    img = tf.io.read_file(img_path)
-    img = tf.image.decode_jpeg(img, channels=3)
-    img = tf.image.convert_image_dtype(img, tf.float32)
-    return img
-
-def extract_delf(image_dataset, store_file="fvecs_delf", extract_size=300):
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
-
-    print(f"[INFO] image_dataset_folder: {image_dataset}, extract_feature_size: {extract_size}")
-
-    binary_file = f"{store_file}.bin"
-    name_file = f"{store_file}_names.txt"
-    print(f"[INFO] binary file: {binary_file}, name_file: {name_file}")
-
-    fnames = glob.glob(image_dataset, recursive=True)
-
-    delf = hub.load('https://tfhub.dev/google/delf/1').signatures['default']
-    with open(binary_file, 'wb') as f:
-        for i, image_path in enumerate(fnames):
-            img = get_image(image_path)
-
-            result = delf(image=img,
-                          score_threshold=tf.constant(100.0),
-                          image_scales=tf.constant([0.25, 0.3536, 0.5, 0.7071, 1.0, 1.4142, 2.0]),
-                          max_feature_num=tf.constant(1000))
-
-            descrip = result['descriptors']
-            descrip = np.array(descrip)
-
-            if descrip.shape[0] < extract_size:
-                print(f"[INFO] skipped {i} image, path: {image_path}")
-                continue
-
-            descrip = descrip[:extract_size]
-
-            # fvecs의 길이 만큼 fmt설정
-            fmt = f'{np.prod(descrip.shape)}f'
-
-            # fmt = 포맷, fvecs.flatten() = 벡터를 한줄로 변환
-            # struct.pack을 사용하여 패킹한다.
-            f.write(struct.pack(fmt, *(descrip.flatten())))
-
-            print(f"[INFO] Process {i}/{len(fnames)} images....., descrip shape: {descrip.shape}")
-
-
+            print(f"[INFO] Process {i * batch_size}/{len(fnames)} images.....")
 
     with open(name_file, 'w') as f:
         f.write('\n'.join(fnames))
 
 if __name__ == '__main__':
-    extract_res('../image/**/*.jpg')
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", required=True,
+                        help='Dataset path format, ex) ./image/**/*.jpg')
+    parser.add_argument("--directory", required=False,
+                        default="features",
+                        help='Features and Image Path store File')
+    parser.add_argument("--store", required=False,
+                        default="fvecs",
+                        help='Features and Image Path store File')
+    args = parser.parse_args()
+
+    extract_res(args.dataset, args.directory, store_file=args.store)
