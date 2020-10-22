9df732e28014720985468ca63c99ec45ff87baa7
이강준 <ladun9128@gmail.com>
2020-10-22 21:03:55 +0900

    간단한 모델 사용법 주석 추가
    Former-commit-id: b15a79e95d01769701448c1692f669f44c95f1b7 [formerly 770f3ad025f8da54093334dc62114a92508227fa] [formerly c765d65acfd61cc8b85fec68748cf99638818602 [formerly d0a4ab6a430fe77052bab5bc3aed65f982950d82]]
    Former-commit-id: a790a3fe4b5f606d3004e23d328cb5ea85feafb9 [formerly e8218bccfa0c46f01587ba6f9cb0aa73ee5ecd1d]
    Former-commit-id: 90f9cecb271a4b6c3123fc09ecf074f0756016d9

--- background_classification/eval.py
+++ background_classification/eval.py
@@ -13,6 +13,35 @@ import background_classification.data.config as config
 #
 # ================================================
 
+'''
+모델 사용방법
+from background_classification.model.model import BGClassification
+
+# 모델 생성하는 부분
+model = BGClassification()
+model.load_weights("path/to/trained_model")
+model.eval()
+
+if cuda:
+    model = model.cuda()
+
+# 모델에 넣을 이미지 불러오기 -> YOLACT랑 똑같이 만듬
+img = cv2.imread(args.image)
+frame = torch.from_numpy(img).float()
+if args.cuda:
+    frame = frame.cuda()
+batch = FastBaseTransform()(frame.unsqueeze(0))
+
+# 실제로 모델 테스트
+with torch.no_grad():
+    outputs = model(batch)
+    # 나온 확률 중 가장 큰 값
+    _, pred = torch.max(outputs, 1)
+    print(config.CLASSES[pred])
+    
+'''
+
+
 def main():
     parse_args()
     evaluate()
