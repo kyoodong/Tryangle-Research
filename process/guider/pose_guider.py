cb6ae59a7f292fe19cdb65ba7bd6e018c6551285
이동규 <wind.dong.dream@gmail.com>
2020-11-02 00:04:38 +0900

    Guider 가 잘 지켜진 가이드를 제공하는 방향으로 수정
    Former-commit-id: 41d6762d5649361fbe27b9f6aac5bd2f7d794e72 [formerly 31b2eaacd390c67db72f6a8a7c740a5c4596188a]
    Former-commit-id: 53d566025fa5991513ce1b16824e36aa2ee96312

--- process/guider/pose_guider.py
+++ process/guider/pose_guider.py
@@ -1,5 +1,6 @@
 from process.object import Human
 from process.pose import HumanPose
+from process.guide import ObjectGuide
 
 
 class PoseGuider:
@@ -34,7 +35,7 @@ class PoseGuider:
         return False
 
     def has_body(self):
-        return self.has_upper_body() or self.has_lower_body()
+        return self.has_upper_body() and self.has_lower_body()
 
     def run(self, image):
         guide_message_list = list()
@@ -46,42 +47,22 @@ class PoseGuider:
 
             # 사람이 사진 밑쪽에 위치한 경우
             if self.human.roi[2] + gamma > height:
-                # 발목이 잘린 경우
-                # 무릎은 있으나 발목, 발꿈치 등이 모두 없는 경우
-                if self.human.pose[Human.BODY_PARTS[Human.Part.LAnkle]][2] <= HumanPose.POSE_THRESHOLD and\
-                    self.human.pose[Human.BODY_PARTS[Human.Part.RAnkle]][2] <= HumanPose.POSE_THRESHOLD and\
+                # Ankle, knee 모두 존재하는 경우 발 끝을 맞춘 것임
+                if self.human.pose[Human.BODY_PARTS[Human.Part.LAnkle]][2] > HumanPose.POSE_THRESHOLD and\
+                    self.human.pose[Human.BODY_PARTS[Human.Part.RAnkle]][2] > HumanPose.POSE_THRESHOLD and\
                         self.human.pose[Human.BODY_PARTS[Human.Part.LKnee]][2] > HumanPose.POSE_THRESHOLD and\
                         self.human.pose[Human.BODY_PARTS[Human.Part.RKnee]][2] > HumanPose.POSE_THRESHOLD:
                     human_height = self.human.roi[2] - self.human.roi[0]
                     diff = -human_height * 10 / 170
-                    guide_message_list.append(ObjectGuide(self.human_component.id, 3, diff, 0))
+                    guide_message_list.append(ObjectGuide(self.human_component.id, 2, diff, 0))
 
-                # 무릎이 잘린 경우
-                if self.human.pose[Human.BODY_PARTS[Human.Part.LKnee]][2] <= HumanPose.POSE_THRESHOLD or\
-                        self.human.pose[Human.BODY_PARTS[Human.Part.RKnee]][2] <= HumanPose.POSE_THRESHOLD:
-                    human_height = self.human.roi[2] - self.human.roi[0]
-                    diff = human_height * 20 / 170
-                    guide_message_list.append(
-                        ObjectGuide(self.human_component.id, 7, diff, 0))
-
+                # 상반신 사진인 경우
                 if self.has_head():
-                    if not self.has_body():
+                    # 하체는 없고 상체만 있음 = 상반신 사진
+                    if self.has_upper_body() and self.has_lower_body():
                         human_height = self.human.roi[2] - self.human.roi[0]
                         diff = -human_height * 20 / 170
                         guide_message_list.append(
                             ObjectGuide(self.human_component.id, 8, diff, 0))
-
-            if height > self.human.roi[2] + self.foot_lower_threshold:
-                guide_message_list.append(
-                    ObjectGuide(self.human_component.id, 2, height - self.human.roi[2] + self.foot_lower_threshold, 0))
-
-            # 사람이 사진의 윗쪽에 위치한 경우
-            if self.human.roi[0] < gamma:
-                # 머리가 있다면
-                if self.has_head():
-                    top = height // 3
-                    diff = top - self.human.roi[0]
-                    guide_message_list.append(
-                        ObjectGuide(self.human_component.id, 9, diff, 0))
 
         return guide_message_list
