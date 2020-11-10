7c7ba6d37a84f6a5c275aed9e8d2a7a2c86ddb9b
kyoodong <wind.dong.dream@gmail.com>
2020-11-11 03:23:58 +0900

    pose
    Former-commit-id: 5cf72b61d643c74c24601a122cf3fe333c56f8f1 [formerly 7f30168263a4156a27f2ea68a60fbd8c28e95694]
    Former-commit-id: 2bbb340b6ac3f5b8206f7405e34efcd57d27c54f

--- codes/score.py
+++ codes/score.py
@@ -13,42 +13,7 @@ N_CLUSTER = 15
 MAX_SCORE = 5
 
 image_input = Input(shape=(None, None, 3), name="image")
-cluster_input = Input(shape=(1, ), name="cluster")
-
-nose_x_input = Input(shape=(1, ), name="nose_x")
-nose_y_input = Input(shape=(1, ), name="nose_y")
-left_eye_x_input = Input(shape=(1, ), name="left_eye_x")
-left_eye_y_input = Input(shape=(1, ), name="left_eye_y")
-right_eye_x_input = Input(shape=(1, ), name="right_eye_x")
-right_eye_y_input = Input(shape=(1, ), name="right_eye_y")
-left_ear_x_input = Input(shape=(1, ), name="left_ear_x")
-left_ear_y_input = Input(shape=(1, ), name="left_ear_y")
-right_ear_x_input = Input(shape=(1, ), name="right_ear_x")
-right_ear_y_input = Input(shape=(1, ), name="right_ear_y")
-left_shoulder_x_input = Input(shape=(1, ), name="left_shoulder_x")
-left_shoulder_y_input = Input(shape=(1, ), name="left_shoulder_y")
-right_shoulder_x_input = Input(shape=(1, ), name="right_shoulder_x")
-right_shoulder_y_input = Input(shape=(1, ), name="right_shoulder_y")
-left_elbow_x_input = Input(shape=(1, ), name="left_elbow_x")
-left_elbow_y_input = Input(shape=(1, ), name="left_elbow_y")
-right_elbow_x_input = Input(shape=(1, ), name="right_elbow_x")
-right_elbow_y_input = Input(shape=(1, ), name="right_elbow_y")
-left_wrist_x_input = Input(shape=(1, ), name="left_wrist_x")
-left_wrist_y_input = Input(shape=(1, ), name="left_wrist_y")
-right_wrist_x_input = Input(shape=(1, ), name="right_wrist_x")
-right_wrist_y_input = Input(shape=(1, ), name="right_wrist_y")
-left_hip_x_input = Input(shape=(1, ), name="left_hip_x")
-left_hip_y_input = Input(shape=(1, ), name="left_hip_y")
-right_hip_x_input = Input(shape=(1, ), name="right_hip_x")
-right_hip_y_input = Input(shape=(1, ), name="right_hip_y")
-left_knee_x_input = Input(shape=(1, ), name="left_knee_x")
-left_knee_y_input = Input(shape=(1, ), name="left_knee_y")
-right_knee_x_input = Input(shape=(1, ), name="right_knee_x")
-right_knee_y_input = Input(shape=(1, ), name="right_knee_y")
-left_ankle_x_input = Input(shape=(1, ), name="left_ankle_x")
-left_ankle_y_input = Input(shape=(1, ), name="left_ankle_y")
-right_ankle_x_input = Input(shape=(1, ), name="right_ankle_x")
-right_ankle_y_input = Input(shape=(1, ), name="right_ankle_y")
+pose_input = Input(shape=(34, ), name="pose")
 
 
 base_model = tf.keras.applications.ResNet101(include_top=False, input_tensor=image_input)
@@ -57,28 +22,20 @@ base_model = tf.keras.applications.ResNet101(include_top=False, input_tensor=ima
 x = base_model.output
 x = tf.keras.layers.BatchNormalization()(x)
 x = GlobalAveragePooling2D()(x)
+x = concatenate([x, pose_input])
 x = Dense(1024, activation='relu')(x)
 x = Dense(64, activation='relu')(x)
-x = concatenate([x, nose_x_input, nose_y_input, left_eye_x_input, left_eye_y_input, right_eye_x_input, right_eye_y_input,
-     left_ear_x_input, left_ear_y_input, right_ear_x_input, right_ear_y_input,
-     left_shoulder_x_input, left_shoulder_y_input, right_shoulder_x_input, right_shoulder_y_input,
-     left_elbow_x_input, left_elbow_y_input, right_elbow_x_input, right_elbow_y_input,
-     left_wrist_x_input, left_wrist_y_input, right_wrist_x_input, right_wrist_y_input,
-     left_hip_x_input, left_hip_y_input, right_hip_x_input, right_hip_y_input,
-     left_knee_x_input, left_knee_y_input, right_knee_x_input, right_knee_y_input,
-     left_ankle_x_input, left_ankle_y_input, right_ankle_x_input, right_ankle_y_input])
 x = Dense(1024, activation='relu')(x)
-x = Dense(1024, activation='relu')(x)
-x = Dense(1024, activation='relu')(x)
+
 prediction = Dense(MAX_SCORE + 1, activation='softmax')(x)
 
 model = Model(
-    inputs=[
-        image_input],
+    inputs=[image_input, pose_input],
     outputs=prediction
 )
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+
 model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy",
               metrics=["sparse_categorical_crossentropy", "acc"])
 
@@ -93,55 +50,11 @@ data = pd.read_csv("data.csv", header=None, names=["url", "id", "object_id", "po
                                                    "left_ankle_x", "left_ankle_y", "right_ankle_x", "right_ankle_y",
                                                    "score"])
 
-id = np.array(data.pop("id"), np.float32)
-object_id = np.array(data.pop("object_id"), np.float32)
-pose_id = np.array(data.pop("pose_id"), np.float32)
-nose_x = np.array(data.pop("nose_x"), np.float32)
-nose_y = np.array(data.pop("nose_y"), np.float32)
-left_eye_x = np.array(data.pop("left_eye_x"), np.float32)
-left_eye_y = np.array(data.pop("left_eye_y"), np.float32)
-right_eye_x = np.array(data.pop("right_eye_x"), np.float32)
-right_eye_y = np.array(data.pop("right_eye_y"), np.float32)
-left_ear_x = np.array(data.pop("left_ear_x"), np.float32)
-left_ear_y = np.array(data.pop("left_ear_y"), np.float32)
-right_ear_x = np.array(data.pop("right_ear_x"), np.float32)
-right_ear_y = np.array(data.pop("right_ear_y"), np.float32)
-left_shoulder_x = np.array(data.pop("left_shoulder_x"), np.float32)
-left_shoulder_y = np.array(data.pop("left_shoulder_y"), np.float32)
-right_shoulder_x = np.array(data.pop("right_shoulder_x"), np.float32)
-right_shoulder_y = np.array(data.pop("right_shoulder_y"), np.float32)
-left_elbow_x = np.array(data.pop("left_elbow_x"), np.float32)
-left_elbow_y = np.array(data.pop("left_elbow_y"), np.float32)
-right_elbow_x = np.array(data.pop("right_elbow_x"), np.float32)
-right_elbow_y = np.array(data.pop("right_elbow_y"), np.float32)
-left_wrist_x = np.array(data.pop("left_wrist_x"), np.float32)
-left_wrist_y = np.array(data.pop("left_wrist_y"), np.float32)
-right_wrist_x = np.array(data.pop("right_wrist_x"), np.float32)
-right_wrist_y = np.array(data.pop("right_wrist_y"), np.float32)
-left_hip_x = np.array(data.pop("left_hip_x"), np.float32)
-left_hip_y = np.array(data.pop("left_hip_y"), np.float32)
-right_hip_x = np.array(data.pop("right_hip_x"), np.float32)
-right_hip_y = np.array(data.pop("right_hip_y"), np.float32)
-left_knee_x = np.array(data.pop("left_knee_x"), np.float32)
-left_knee_y = np.array(data.pop("left_knee_y"), np.float32)
-right_knee_x = np.array(data.pop("right_knee_x"), np.float32)
-right_knee_y = np.array(data.pop("right_knee_y"), np.float32)
-left_ankle_x = np.array(data.pop("left_ankle_x"), np.float32)
-left_ankle_y = np.array(data.pop("left_ankle_y"), np.float32)
-right_ankle_x = np.array(data.pop("right_ankle_x"), np.float32)
-right_ankle_y = np.array(data.pop("right_ankle_y"), np.float32)
+poses = np.array(data.loc[:, "nose_x": "right_ankle_y"], np.float32)
 labels = np.array(data.pop("score"), np.float32)
-urls = np.array(data).reshape([-1])
+urls = np.array(data.pop("url")).reshape([-1])
 
-train_dataset = tf.data.Dataset.from_tensor_slices(
-    (urls, nose_x, nose_y, left_eye_x, left_eye_y, right_eye_x, right_eye_y,
-     left_ear_x, left_ear_y, right_ear_x, right_ear_y,
-     left_shoulder_x, left_shoulder_y, right_shoulder_x, right_shoulder_y,
-     left_elbow_x, left_elbow_y, right_elbow_x, right_elbow_y,
-     left_wrist_x, left_wrist_y, right_wrist_x, right_wrist_y,
-     left_hip_x, left_hip_y, right_hip_x, right_hip_y,
-     left_knee_x, left_knee_y, right_knee_x, right_knee_y,
-     left_ankle_x, left_ankle_y, right_ankle_x, right_ankle_y, labels))
+train_dataset = tf.data.Dataset.from_tensor_slices((urls, poses, labels))
 
 
 data = pd.read_csv("test_data.csv", header=None, names=["url", "id", "object_id", "pose_id", "nose_x", "nose_y",
@@ -155,61 +68,16 @@ data = pd.read_csv("test_data.csv", header=None, names=["url", "id", "object_id"
                                                    "left_ankle_x", "left_ankle_y", "right_ankle_x", "right_ankle_y",
                                                    "score"])
 
-id = np.array(data.pop("id"), np.float32)
-object_id = np.array(data.pop("object_id"), np.float32)
-pose_id = np.array(data.pop("pose_id"), np.float32)
-nose_x = np.array(data.pop("nose_x"), np.float32)
-nose_y = np.array(data.pop("nose_y"), np.float32)
-left_eye_x = np.array(data.pop("left_eye_x"), np.float32)
-left_eye_y = np.array(data.pop("left_eye_y"), np.float32)
-right_eye_x = np.array(data.pop("right_eye_x"), np.float32)
-right_eye_y = np.array(data.pop("right_eye_y"), np.float32)
-left_ear_x = np.array(data.pop("left_ear_x"), np.float32)
-left_ear_y = np.array(data.pop("left_ear_y"), np.float32)
-right_ear_x = np.array(data.pop("right_ear_x"), np.float32)
-right_ear_y = np.array(data.pop("right_ear_y"), np.float32)
-left_shoulder_x = np.array(data.pop("left_shoulder_x"), np.float32)
-left_shoulder_y = np.array(data.pop("left_shoulder_y"), np.float32)
-right_shoulder_x = np.array(data.pop("right_shoulder_x"), np.float32)
-right_shoulder_y = np.array(data.pop("right_shoulder_y"), np.float32)
-left_elbow_x = np.array(data.pop("left_elbow_x"), np.float32)
-left_elbow_y = np.array(data.pop("left_elbow_y"), np.float32)
-right_elbow_x = np.array(data.pop("right_elbow_x"), np.float32)
-right_elbow_y = np.array(data.pop("right_elbow_y"), np.float32)
-left_wrist_x = np.array(data.pop("left_wrist_x"), np.float32)
-left_wrist_y = np.array(data.pop("left_wrist_y"), np.float32)
-right_wrist_x = np.array(data.pop("right_wrist_x"), np.float32)
-right_wrist_y = np.array(data.pop("right_wrist_y"), np.float32)
-left_hip_x = np.array(data.pop("left_hip_x"), np.float32)
-left_hip_y = np.array(data.pop("left_hip_y"), np.float32)
-right_hip_x = np.array(data.pop("right_hip_x"), np.float32)
-right_hip_y = np.array(data.pop("right_hip_y"), np.float32)
-left_knee_x = np.array(data.pop("left_knee_x"), np.float32)
-left_knee_y = np.array(data.pop("left_knee_y"), np.float32)
-right_knee_x = np.array(data.pop("right_knee_x"), np.float32)
-right_knee_y = np.array(data.pop("right_knee_y"), np.float32)
-left_ankle_x = np.array(data.pop("left_ankle_x"), np.float32)
-left_ankle_y = np.array(data.pop("left_ankle_y"), np.float32)
-right_ankle_x = np.array(data.pop("right_ankle_x"), np.float32)
-right_ankle_y = np.array(data.pop("right_ankle_y"), np.float32)
+poses = np.array(data.loc[:, "nose_x": "right_ankle_y"], np.float32)
 labels = np.array(data.pop("score"), np.float32)
-urls = np.array(data).reshape([-1])
+urls = np.array(data.pop("url")).reshape([-1])
 
 unit_size = data.shape[0] // 10
 
 # 80%
 test_size = unit_size * 8
 
-test_dataset = tf.data.Dataset.from_tensor_slices(
-    (urls, nose_x, nose_y, left_eye_x, left_eye_y, right_eye_x, right_eye_y,
-     left_ear_x, left_ear_y, right_ear_x, right_ear_y,
-     left_shoulder_x, left_shoulder_y, right_shoulder_x, right_shoulder_y,
-     left_elbow_x, left_elbow_y, right_elbow_x, right_elbow_y,
-     left_wrist_x, left_wrist_y, right_wrist_x, right_wrist_y,
-     left_hip_x, left_hip_y, right_hip_x, right_hip_y,
-     left_knee_x, left_knee_y, right_knee_x, right_knee_y,
-     left_ankle_x, left_ankle_y, right_ankle_x, right_ankle_y, labels)
-)
+test_dataset = tf.data.Dataset.from_tensor_slices((urls, poses, labels))
 val_dataset = test_dataset.skip(test_size)
 
 normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
@@ -220,33 +88,11 @@ def decode_img(img):
     return tf.image.resize(img, [224, 224])
 
 
-def preprocess_data(url, nose_x, nose_y, left_eye_x, left_eye_y, right_eye_x, right_eye_y,
-     left_ear_x, left_ear_y, right_ear_x, right_ear_y,
-     left_shoulder_x, left_shoulder_y, right_shoulder_x, right_shoulder_y,
-     left_elbow_x, left_elbow_y, right_elbow_x, right_elbow_y,
-     left_wrist_x, left_wrist_y, right_wrist_x, right_wrist_y,
-     left_hip_x, left_hip_y, right_hip_x, right_hip_y,
-     left_knee_x, left_knee_y, right_knee_x, right_knee_y,
-     left_ankle_x, left_ankle_y, right_ankle_x, right_ankle_y, label):
+def preprocess_data(url, pose, label):
     image = tf.io.read_file(url)
     image = decode_img(image)
     image = normalization_layer(image)
-    return {"image": image, "nose_x": nose_x, "nose_y": nose_y, "left_eye_x": left_eye_x, "left_eye_y": left_eye_y,
-            "right_eye_x": right_eye_x, "right_eye_y": right_eye_y,
-            "left_ear_x": left_ear_x, "left_ear_y": left_ear_y,
-            "right_ear_x": right_ear_x, "right_ear_y": right_ear_y,
-            "left_shoulder_x": left_shoulder_x, "left_shoulder_y": left_shoulder_y,
-            "right_shoulder_x": right_shoulder_x, "right_shoulder_y": right_shoulder_y,
-            "left_elbow_x": left_elbow_x, "left_elbow_y": left_elbow_y,
-            "right_elbow_x": right_elbow_x, "right_elbow_y": right_elbow_y,
-            "left_wrist_x": left_wrist_x, "left_wrist_y": left_wrist_y,
-            "right_wrist_x": right_wrist_x, "right_wrist_y": right_wrist_y,
-            "left_hip_x": left_hip_x, "left_hip_y": left_hip_y,
-            "right_hip_x": right_hip_x, "right_hip_y": right_hip_y,
-            "left_knee_x": left_knee_x, "left_knee_y": left_knee_y,
-            "right_knee_x": right_knee_x, "right_knee_y": right_knee_y,
-            "left_ankle_x": left_ankle_x, "left_ankle_y": left_ankle_y,
-            "right_ankle_x": right_ankle_x, "right_ankle_y": right_ankle_y}, label
+    return {"image": image, "pose": pose}, label
 
 
 BATCH_SIZE = 4
